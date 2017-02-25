e0f48473d9c9050c776ebddd62267e093f2ebd7a
Noah Scholfield <contact@noahscholfield.com>
2017-02-25 14:29:25 -0500

    Add Bootstrap

--- example3/script.js
+++ example3/script.js
@@ -1,10 +1,11 @@
 (function() {
   const form = document.querySelector('#myForm');
   const dropdown = document.querySelector('#dropdown');
+  const textbox = document.querySelector('#trumpsbutt');
   const message = 'Donald Trump is ';
 
   function showValue(e) {
-    alert(message + dropdown.value);
+    alert(message + dropdown.value + '\n' + textbox.value);
     e.preventDefault();
   }
 
